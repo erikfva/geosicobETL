743dd7d7a5968ff1f3b1d0ee3ba404f3559de1aa
Erik Vargas Agreda <erik.vargas@yandex.com>
2017-07-04 20:35:02 -0400

    Desmontes inscrito en ley 337

---
+++ import_desmontes_337.bat
@@ -0,0 +1,22 @@
+@echo off
+call config.bat
+REM ********************************************************************************
+REM **Importando Autorizaciones Transitorias Especiales (ATE) de la cobertura de la geodatabase UMIG ABT**
+REM ********************************************************************************
+
+set lyrsearch=D0603402_DESMONTES_INSCRITOS
+set lyrname=
+call dosearch.bat
+
+%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -lco FID=sicob_id -overwrite -progress --config PG_USE_COPY YES -nln d337 %lyrname%
+REM ** Eliminando campos innecesarios **
+(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid_1) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid_12) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS shape_area) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS shape_length) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+REM ** Agregando campos del geoSICOB **
+(echo SELECT sicob_add_geoinfo_column('%pgschema%.d337'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo SELECT sicob_update_geoinfo_column('%pgschema%.d337'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+REM ** Eliminando polígonos con geometrías vacias **
+(echo DELETE FROM %pgschema%.d337 WHERE sicob_sup IS NULL;) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
