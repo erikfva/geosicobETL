db0ecc2946d591baa38124e6562e5a0b8fe694c3
Erik Vargas Agreda <erik.vargas@yandex.com>
2017-07-19 18:44:18 -0400

    Se corriguió la referencia en la eliminación de campos innesesarios.

--- import_desmontes_337.bat
+++ import_desmontes_337.bat
@@ -10,11 +10,11 @@ call dosearch.bat
 
 %PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -lco FID=sicob_id -overwrite -progress --config PG_USE_COPY YES -nln d337 %lyrname%
 REM ** Eliminando campos innecesarios **
-(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid_1) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS objectid_12) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS shape_area) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.ate DROP COLUMN IF EXISTS shape_length) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.d337 DROP COLUMN IF EXISTS objectid) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.d337 DROP COLUMN IF EXISTS objectid_1) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.d337 DROP COLUMN IF EXISTS objectid_12) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.d337 DROP COLUMN IF EXISTS shape_area) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+(echo ALTER TABLE %pgschema%.d337 DROP COLUMN IF EXISTS shape_length) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 REM ** Agregando campos del geoSICOB **
 (echo SELECT sicob_add_geoinfo_column('%pgschema%.d337'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo SELECT sicob_update_geoinfo_column('%pgschema%.d337'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
