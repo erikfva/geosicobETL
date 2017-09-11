eed39d1336198c395b3f41a3b36047a4ebb4e101
Erik Vargas Agreda <erik.vargas@yandex.com>
2017-09-11 11:20:03 -0400

    Debido a que en la fuente no incluye el campo idpredio, tiene que ser generado a partir de agrupamiento por nombre de predio y propietario.

--- import_titulado.bat
+++ import_titulado.bat
@@ -20,37 +20,15 @@ call dosearch.bat
 set Zona21=%lyrname%
 
 set lyrname=parcelas_tituladas
+set pgschema=temp
 
-%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -lco FID=sicob_id -overwrite -progress --config PG_USE_COPY YES -nln %lyrname% %Zona19%
-%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -append -progress --config PG_USE_COPY YES -nln %lyrname% %Zona20%
-%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -append -progress --config PG_USE_COPY YES -nln %lyrname% %Zona21%
+set sql=SELECT numerodocu AS titulo, fechadocum AS fecha_titulo, parcela AS predio, nombreplan AS propietario, clasificac AS tipo_propiedad, sup_cc, superficie AS sup_predio, clasetitul, calificaci, (parcela+nombreplan) AS infopredio
+%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -lco FID=sicob_id -overwrite -progress --config PG_USE_COPY YES -nln %lyrname% -sql "%sql% FROM %Zona21%"
+%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -append -progress --config PG_USE_COPY YES -nln %lyrname% -sql "%sql% FROM %Zona20%"
+%PATH_OGR2OGR% -f "PostgreSQL" PG:"host=%pghost% user=%pguser% port=%pgport% dbname=%pgdb% ACTIVE_SCHEMA=%pgschema%" "%PATH_GDB%" -lco GEOMETRY_NAME=%pggeom% -append -progress --config PG_USE_COPY YES -nln %lyrname% -sql "%sql% FROM %Zona19%"
 
-REM ** Eliminando campos innecesarios **
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS objectid) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS objectid_1) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS objectid_12) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS tipoobjeto) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS shape_area) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS shape_length) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% DROP COLUMN IF EXISTS shape_leng) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+REM ** Adicionando índice de infopredio**
+(echo CREATE INDEX %lyrname%_idx_infopredio ON %pgschema%.%lyrname% USING btree (infopredio^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 
-REM ** Corrigiendo errores **
-(echo UPDATE %pgschema%.%lyrname% SET the_geom = ST_Multi(ST_CollectionExtract(st_makevalid(the_geom^^^),3^^^)^^^) WHERE st_isvalid(the_geom^^^) = 'f') | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-
-REM ** Renombrando campos para su uso en el geoSICOB **
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN numerodocu TO titulo) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN parcela TO predio) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN nombreplan TO propietario) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN fechadocum TO fecha_titulo) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN clasificac TO tipo_propiedad) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo ALTER TABLE %pgschema%.%lyrname% RENAME COLUMN superficie TO sup_predio) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-REM ** Adicionando índices **
-(echo CREATE INDEX %lyrname%_idx_idpredio ON %pgschema%.%lyrname% USING btree (idpredio^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo CREATE INDEX %lyrname%_idx_titulo ON %pgschema%.%lyrname% USING btree (titulo^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo CREATE INDEX %lyrname%_idx_predio ON %pgschema%.%lyrname% USING btree (predio^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo CREATE INDEX %lyrname%_idx_propietario ON %pgschema%.%lyrname% USING btree (propietario^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-(echo CREATE INDEX %lyrname%_idx_tipo_propiedad ON %pgschema%.%lyrname% USING btree (tipo_propiedad^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-
+REM ** Ejecutando archivo de script SQL
 psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb% -f predios_titulados.sql
-
-REM (echo CREATE OR REPLACE VIEW coberturas.predios_titulados AS SELECT c.sicob_id, c.numerodocu AS titulo, c.parcela AS predio, c.nombreplan AS propietario, c.fechadocum AS fecha_titulo, c.clasificac AS tipo_propiedad, c.sup_cc AS sup_predio, NULL::text AS resol_pop, NULL::timestamp with time zone AS fec_resol_pop, c.the_geom FROM coberturas.predios_titulados_geo_inra c;) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
--- predios_titulados.sql
+++ predios_titulados.sql
@@ -1,11 +1,48 @@
+--> Creando cobertura de parcelas_tituladas
+DROP TABLE IF EXISTS coberturas.parcelas_tituladas;
+create table coberturas.parcelas_tituladas as
+SELECT min(a.sicob_id) OVER w AS idpredio, count(a.infopredio) OVER w AS parcelas,
+a.* 
+FROM temp.parcelas_tituladas a
+ WINDOW w AS (PARTITION BY a.infopredio);
+
+-->Eliminando archivo temporal
+DROP TABLE temp.parcelas_tituladas CASCADE;
+
+/** Corrigiendo errores **/
+UPDATE coberturas.parcelas_tituladas SET the_geom = ST_Multi(ST_CollectionExtract(st_makevalid(the_geom),3)) WHERE st_isvalid(the_geom) = 'f';
+
+-->** Adicionando índices **
+DROP INDEX IF EXISTS  coberturas.parcelas_tituladas_idx_idpredio;
+CREATE INDEX parcelas_tituladas_idx_idpredio ON coberturas.parcelas_tituladas USING btree (idpredio);
+DROP INDEX IF EXISTS coberturas.parcelas_tituladas_idx_titulo;
+CREATE INDEX parcelas_tituladas_idx_titulo ON coberturas.parcelas_tituladas USING btree (titulo);
+DROP INDEX IF EXISTS  coberturas.parcelas_tituladas_idx_predio;
+CREATE INDEX parcelas_tituladas_idx_predio ON coberturas.parcelas_tituladas USING btree (predio);
+DROP INDEX IF EXISTS  coberturas.parcelas_tituladas_idx_propietario;
+CREATE INDEX parcelas_tituladas_idx_propietario ON coberturas.parcelas_tituladas USING btree (propietario);
+DROP INDEX IF EXISTS  coberturas.parcelas_tituladas_idx_tipo_propiedad;
+CREATE INDEX parcelas_tituladas_idx_tipo_propiedad ON coberturas.parcelas_tituladas USING btree (tipo_propiedad);
+DROP INDEX IF EXISTS  coberturas.parcelas_tituladas_geom_idx;
+CREATE INDEX parcelas_tituladas_geom_idx ON coberturas.parcelas_tituladas
+  USING gist (the_geom public.gist_geometry_ops_2d);
+ALTER TABLE coberturas.parcelas_tituladas DROP CONSTRAINT IF EXISTS parcelas_tituladas_pkey;
+ALTER TABLE coberturas.parcelas_tituladas ADD PRIMARY KEY (sicob_id);
+
 -->Creando cobertura de predios titulados agrupando las parcelas por idpredio
 DROP TABLE IF EXISTS coberturas.predios_titulados;
 create table coberturas.predios_titulados as
-SELECT idpredio,predio, propietario, string_agg(t.titulo , ',') as titulo, string_agg(t.fecha_titulo , ',') as fecha_titulo, string_agg(t.clasetitul , ',') as clasetitul, min(t.calificaci) as calificaci, min(t.tipo_propiedad) as tipo_propiedad, sum(sup_predio) as sup_predio, sum(parcelas) as parcelas, ST_CollectionExtract(st_collect(the_geom),3) as the_geom
+SELECT row_number() over() AS sicob_id, idpredio,predio, propietario, string_agg(titulo , ',') as titulo, 
+	string_agg(fecha_titulo , ',') as fecha_titulo, string_agg(clasetitul , ',') as clasetitul, min(calificaci) as calificaci, 
+	min(tipo_propiedad) as tipo_propiedad, max(sup_predio) as sup_predio, max(parcelas) as parcelas,
+	(SELECT ST_Multi(ST_CollectionExtract(st_collect(the_geom),3)) as the_geom 
+		FROM coberturas.parcelas_tituladas u WHERE u.idpredio =t.idpredio
+	) AS the_geom
 FROM (
-	SELECT idpredio,predio, propietario, titulo, to_char(fecha_titulo,'DD/MM/YYYY') AS fecha_titulo, clasetitul, calificaci, tipo_propiedad, max(sup_predio) as sup_predio, count(idpredio) as parcelas, st_collect(the_geom) as the_geom
-	FROM coberturas.parcelas_tituladas t
-	GROUP BY idpredio,predio,propietario,titulo, fecha_titulo, clasetitul, calificaci, tipo_propiedad
+	SELECT DISTINCT ON (idpredio,titulo)
+	idpredio ,predio, propietario, titulo, to_char(fecha_titulo,'DD/MM/YYYY') AS fecha_titulo, clasetitul, calificaci, tipo_propiedad, sup_predio,parcelas
+	FROM coberturas.parcelas_tituladas 
+	order by idpredio,titulo
 ) t
 GROUP BY t.idpredio,t.predio,t.propietario;
 
@@ -17,8 +54,7 @@ CREATE INDEX predios_titulados_idx_propietario ON coberturas.predios_titulados U
 CREATE INDEX predios_titulados_idx_tipo_propiedad ON coberturas.predios_titulados USING btree (tipo_propiedad);
 CREATE INDEX predios_titulados_the_geom_geom_idx ON coberturas.predios_titulados
   USING gist (the_geom public.gist_geometry_ops_2d);
--->Agregando campos del geoSICOB
-SELECT sicob_create_id_column('coberturas.predios_titulados');
+ALTER TABLE coberturas.predios_titulados ADD PRIMARY KEY (sicob_id);
 
 -->CREANDO EL DETALLE DE LOS ELEMENTOS CON ERRORES (BORDES SOBREPUESTOS)
 DROP TABLE IF EXISTS temp.predios_titulados_to_fix;
@@ -35,4 +71,7 @@ FROM
   (select x.sicob_id, (st_dump(x.the_geom)).geom as the_geom from coberturas.predios_titulados x inner join temp.predios_titulados_to_fix y on (x.sicob_id = y.sicob_id) ) c
   group by sicob_id
 ) b
-where a.sicob_id = b.sicob_id
+where a.sicob_id = b.sicob_id;
+
+--> Eliminando archivo temporal
+DROP TABLE IF EXISTS temp.predios_titulados_to_fix CASCADE;
