6a67c51972124766d06341c5bf103468ff01a2fc
Erik Vargas Agreda <erik.vargas@yandex.com>
2017-10-04 12:13:47 -0400

    Se modificó la función para importar PDM corrigiendo geometrías inválidas.

--- import_pdm.bat
+++ import_pdm.bat
@@ -15,13 +15,15 @@ REM ** Eliminando campos innecesarios **
 (echo ALTER TABLE %pgschema%.pdm DROP COLUMN IF EXISTS objectid_12) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo ALTER TABLE %pgschema%.pdm DROP COLUMN IF EXISTS shape_area) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo ALTER TABLE %pgschema%.pdm DROP COLUMN IF EXISTS shape_length) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
+REM ** Corrigiendo errores **
+(echo UPDATE %pgschema%.pdm SET the_geom = ST_Multi(ST_CollectionExtract(st_makevalid(the_geom^^^),3^^^)^^^) WHERE st_isvalid(the_geom^^^) = 'f') | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 REM ** Agregando campos del geoSICOB **
 (echo SELECT sicob_create_id_column('%pgschema%.pdm'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo SELECT sicob_add_geoinfo_column('%pgschema%.pdm'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo SELECT sicob_update_geoinfo_column('%pgschema%.pdm'^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-REM ** Eliminando polígonos con geometrías vacias **
+REM ** Eliminando polï¿½gonos con geometrï¿½as vacias **
 (echo DELETE FROM %pgschema%.pdm WHERE sicob_sup IS NULL;) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
-REM ** Adicionando índices **
+REM ** Adicionando ï¿½ndices **
 (echo CREATE INDEX pdm_idx_res_adm ON coberturas.pdm USING btree (res_adm^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo CREATE INDEX pdm_idx_nom_pre ON coberturas.pdm USING btree (nom_pre^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
 (echo CREATE INDEX pdm_idx_nom_pro ON coberturas.pdm USING btree (nom_pro^^^);) | psql -h %pghost% -p %pgport% -U %pguser% -d %pgdb%
