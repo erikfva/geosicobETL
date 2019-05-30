38616f2dd6fbaccff232428e5eca81cca0e9ac07
Erik Vargas <erik.vargas@yandex.com>
2019-05-30 14:44:24 -0400

    Se agrego indices para la capa de servidumbres ecologicas.

--- servidumbres_ecologicas.sql
+++ servidumbres_ecologicas.sql
@@ -1,6 +1,6 @@
 -->Creando cobertura de servidumbres ecologicas mediante "merge" de las capas
 --> selpas, seltum y seltpfp.
-DROP TABLE IF EXISTS coberturas.sel;
+DROP TABLE IF EXISTS coberturas.sel CASCADE;
 CREATE TABLE coberturas.sel AS
 SELECT * FROM coberturas.selpas
 NATURAL FULL OUTER JOIN (SELECT * FROM coberturas.seltum) t2 
@@ -8,3 +8,8 @@ NATURAL FULL OUTER JOIN (SELECT * FROM coberturas.seltpfp) t3;
 DROP TABLE IF EXISTS coberturas.selpas CASCADE;
 DROP TABLE IF EXISTS coberturas.seltpfp CASCADE;
 DROP TABLE IF EXISTS coberturas.seltum CASCADE;
+-->Agregando campos del geoSICOB
+SELECT sicob_create_id_column('coberturas.sel');
+-->Adicionando indices
+CREATE INDEX sel_the_geom_geom_idx ON coberturas.sel
+  USING gist (the_geom public.gist_geometry_ops_2d);
